cafef1fdc4ad9ef8060625c2e9cea3f55102b624
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-06-20 20:41:20 -0300

    Update CaixeiroViajanteProgramacaoDinamica.java

--- CaixeiroViajanteProgramacaoDinamica.java
+++ CaixeiroViajanteProgramacaoDinamica.java
@@ -1,41 +1,61 @@
-public class CaixeiroViajanteProgramacaoDinamica {
-    private final int MAX = 9999;
-    private int numeroVertices;
-    private double tamanho;
-    private int[][] distance;
-    private int completedVisit;
-    private int[][] DP;
+public class CaixeiroViajanteProgramacaoDinamica{
+    
+    private int numero_vertices;
+    
+    private int visita_completa;
+    
+    private int[][] grafo;
+    
+    private int[][] grafo_pd;
 
     public CaixeiroViajanteProgramacaoDinamica(int[][] matriz) {
-        this.numeroVertices = matriz.length;
-        this.tamanho = (double) this.numeroVertices;
-        this.distance = matriz;
-        this.completedVisit = (1 << this.numeroVertices) - 1;
-        this.DP = new int[(int) Math.pow(2.0, this.tamanho)][this.numeroVertices * 2];
-        for (int i = 0; i < (1 << this.numeroVertices); i++) {
-            for (int j = 0; j < this.numeroVertices; j++) {
-                DP[i][j] = -1;
+        
+        this.numero_vertices = matriz.length;
+        
+        this.grafo = matriz;
+        
+        this.visita_completa = (1 << this.numero_vertices) - 1;
+        
+        //2^n pelo tamanho (casas) dos bits à serem utilizados na análise
+        this.grafo_pd = 
+            new int[(int) Math.pow(2.0, this.numero_vertices)][this.numero_vertices * 2];
+        
+        for (int i = 0; i < (1 << this.numero_vertices); i++) {
+            for (int j = 0; j < this.numero_vertices; j++) {
+                
+                grafo_pd[i][j] = -1;
             }
         }
 
     }
 
-    public int valorMenorDistancia(int mark, int position) {
+    public int valorMenorDistancia (int marco_visita, int posicao_atual) {
 
-        if (mark == completedVisit) {
-            return distance[position][0];
+        if (marco_visita == this.visita_completa) {
+            return this.grafo[posicao_atual][0];
         }
-        if (this.DP[mark][position] != -1) {
-            return DP[mark][position];
+
+        if (this.grafo_pd[marco_visita][posicao_atual] != -1) {
+            return this.grafo_pd[marco_visita][posicao_atual];
         }
-        int menorDistancia = MAX;
-        for (int city = 0; city < this.numeroVertices; city++) {
-            if ((mark & (1 << city)) == 0) {
-                int aux = distance[position][city] + valorMenorDistancia(mark | (1 << city), city);
-                menorDistancia = Math.min(menorDistancia, aux);
+
+        int menor_distancia = Integer.MAX_VALUE;
+
+        for (int i = 0; i < this.numero_vertices; i++) {
+
+            int calculo = (1 << i);
+            
+            if ((marco_visita & calculo) == 0) {
+
+                int aux = this.grafo[posicao_atual][i] + valorMenorDistancia(marco_visita | calculo, i);
+                
+                menor_distancia = Math.min(menor_distancia, aux);
             }
+
         }
-        DP[mark][position] = menorDistancia;
-        return menorDistancia;
+
+        this.grafo_pd[marco_visita][posicao_atual] = menor_distancia;
+        
+        return menor_distancia;
     }
 }
